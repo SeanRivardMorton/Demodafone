2675e3fe8799953c8aa5f19aaf465e1bbfa7628c
Sean <sean.rivard.morton@gmail.com>
2018-05-31 18:16:42 -0400

    ProductDescription now accepts PhoneData prop and passes down to children

--- src/components/ProductPicker/ProductDescription.vue
+++ src/components/ProductPicker/ProductDescription.vue
@@ -1,27 +1,32 @@
 <template>
   <div class="home">
-    <h1>Apple iPhone 8</h1>
-    <gold-stars></gold-stars>
-    <p>All-Glass design, advanced cameras, wireless charging and a smart A11 Bionic chip.</p>
-    <p>Intelligence never looked so good.</p>
-    <product-color-picker></product-color-picker>
-    <product-price-summary></product-price-summary>
+    <h1>{{ phoneData.groupName }}</h1>
+    <gold-stars :rating="rating"></gold-stars>
+    <p>{{ displayDescription }}</p>
   </div>
 </template>
 
 <script lang="ts">
 /* eslint-disable import/no-unresolved */
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from 'vue-property-decorator';
 import GoldStars from '@/components/General/GoldStars.vue';
-import ProductOptionsPicker from '@/components/ProductPicker/ProductOptionsPicker.vue';
-import ProductPriceSummary from '@/components/ProductPicker/ProductPriceSummary.vue';
 
 @Component({
   components: {
   GoldStars,
-  ProductOptionsPicker,
-  ProductPriceSummary
+
   }
   })
-export default class ProductDescription extends Vue {}
+export default class ProductDescription extends Vue {
+  @Prop() phoneData!: any
+
+  get rating() {
+    return Math.ceil(this.phoneData.rating);
+  }
+
+  get displayDescription() {
+    return this.phoneData.deviceSummary[0].displayDescription;
+  }
+
+}
 </script>
