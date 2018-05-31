005c045c5b821d11981505dc7707b7c9be7b9db2
Sean <sean.rivard.morton@gmail.com>
2018-05-31 18:15:46 -0400

    Added phone data to root component

--- src/components/ProductPicker.vue
+++ src/components/ProductPicker.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="productPicker">
-    <product-description></product-description>
+    <product-description :phoneData="phoneData"></product-description>
+    <product-options-picker :phoneOptions="phoneOptions"></product-options-picker>
+    <product-price-summary></product-price-summary>
   </div>
 </template>
 
@@ -8,13 +10,29 @@
 /* eslint-disable import/no-unresolved */
 import { Component, Vue } from 'vue-property-decorator';
 import ProductDescription from '@/components/ProductPicker/ProductDescription.vue';
+import ProductOptionsPicker from '@/components/ProductPicker/ProductOptionsPicker.vue';
+import ProductPriceSummary from '@/components/ProductPicker/ProductPriceSummary.vue';
+
+const PHONE_DATA = require('../assets/data/phones.json');
 
 @Component({
   components: {
   ProductDescription,
+  ProductOptionsPicker,
+  ProductPriceSummary
   },
   })
-export default class ProductPicker extends Vue {}
+export default class ProductPicker extends Vue {
+    phoneData = PHONE_DATA[0]
+
+    model = 0
+    currentModel = this.phoneData.deviceSummary[this.model]
+    phoneOptions = {
+      colourName: this.currentModel.colourName,
+      colourHex: this.currentModel.colourHex,
+      memory: this.currentModel.memory,
+    }
+}
 </script>
 
 <style lang='scss' scoped>
