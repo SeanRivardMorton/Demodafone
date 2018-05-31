abef43137f7578d5a31d60ffcf6a96e620f6c33b
Sean <sean.rivard.morton@gmail.com>
2018-05-31 17:03:17 -0400

    New base component for products

---
+++ src/components/ProductPicker.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="productPicker">
+    <product-description></product-description>
+  </div>
+</template>
+
+<script lang="ts">
+/* eslint-disable import/no-unresolved */
+import { Component, Vue } from 'vue-property-decorator';
+import ProductDescription from '@/components/ProductPicker/ProductDescription.vue';
+
+@Component({
+  components: {
+  ProductDescription,
+  },
+  })
+export default class ProductPicker extends Vue {}
+</script>
+
+<style lang='scss' scoped>
+.productPicker {
+  text-align: left;
+}
+</style>
