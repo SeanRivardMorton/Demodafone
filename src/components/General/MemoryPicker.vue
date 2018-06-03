f0d5a21bf3cf795a533298d7452a76438c419708
Sean <sean.rivard.morton@gmail.com>
2018-06-02 23:50:23 -0400

    removed unused memoryOptions interface

--- src/components/General/MemoryPicker.vue
+++ src/components/General/MemoryPicker.vue
@@ -18,10 +18,6 @@
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import { Getter, Action } from 'vuex-class';
 
-interface memoryOptions {
-
-}
-
 @Component
 export default class MemoryPicker extends Vue {
     @Prop() private memoryOptions!: Array<string>;
