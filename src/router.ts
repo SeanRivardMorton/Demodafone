43d2b802d98233f07950c20a15aee7f3213b4242
Sean <sean.rivard.morton@gmail.com>
2018-06-03 12:39:15 -0400

    Linked SCSS

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
-import About from './views/About.vue';
 import BuyPhone from './views/BuyPhone.vue';
 
 Vue.use(Router);
@@ -11,12 +9,7 @@ export default new Router({
     {
       path: '/',
       name: 'BuyPhone',
-      component: BuyPhone,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About,
-    },
-  ],
+      component: BuyPhone
+    }
+  ]
 });
