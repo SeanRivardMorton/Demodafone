87a0ed21748842dd13b7fcd3e2fd29389c0367a7
Sean <sean.rivard.morton@gmail.com>
2018-06-03 12:38:30 -0400

    Refactored CSS

--- src/components/General/ColorPicker.vue
+++ src/components/General/ColorPicker.vue
@@ -5,10 +5,8 @@
     </p>
     <div v-for="color in colorOptions" :key="color.id" class="inline">
       <div :class="{selected: color.active, border: true}">
-        <!-- <div class="gradient"> -->
-        <div class="select" :style="{ backgroundColor: color.hex }" @click="setColor(color.color)">
+        <div class="select gradient" :style="{ backgroundColor: color.hex }" @click="setColor(color.color)">
         </div>
-        <!-- </div> -->
       </div>
     </div>
   </div>
@@ -32,42 +30,9 @@ export default class ColorPicker extends Vue {
 </script>
 
 <style lang='scss' scoped>
-.inline {
-  display: inline;
-}
-
-.border {
-  height: 45px;
-  width: 45px;
-  //   background-color: red;
-  //   margin: 0.5rem;
-  display: inline-block;
-  border-radius: 10px;
-  border: 1px solid transparent;
-}
-
-.selected {
-  border-radius: 10px;
-  border: 1.5px solid green;
-}
-
-$height: 38px;
-
-.select {
-  margin: -42% auto;
-  /* display: inline-block; */
-  top: 50%;
-  position: relative;
-  width: $height;
-  height: $height;
-  border-radius: 7px;
-  -webkit-box-shadow: 0px 0px 1px 0px black;
-  box-shadow: 0px 0px 1px 0px black;
-  /* position: absolute; */
-  z-index: 0;
+.gradient {
   &:after {
     content: '';
-    display: block;
     position: absolute;
     width: 100%;
     height: 45%;
@@ -78,29 +43,4 @@ $height: 38px;
     background: linear-gradient(transparent, rgba(0, 0, 0, 0.315)) left repeat;
   }
 }
-
-// .gradient:after {
-//   //   height: 100%;
-//   //   width: 100%;
-//   //   position: relative;
-//   //   z-index: 10;
-//   position: relative;
-//   display: inline-block;
-//   &:after {
-//     content: '';
-//     display: block;
-//     position: absolute;
-//     width: 100%;
-//     height: 55%;
-//     bottom: 0;
-//     // background: -webkit-linear-gradient(transparent, #f8e7dc) left repeat;
-//     background: linear-gradient(transparent, black) left repeat;
-//     // background: linear-gradient(
-//     //   to bottom,
-//     //   rgba(0, 0, 0, 0) 0%,
-//     //   rgba(0, 0, 0, 0) 59%,
-//     //   rgba(0, 0, 0, 0.65) 100%
-//     // );
-//   }
-// }
 </style>
--- src/components/General/MemoryPicker.vue
+++ src/components/General/MemoryPicker.vue
@@ -32,53 +32,10 @@ export default class MemoryPicker extends Vue {
 </script>
 
 <style lang='scss' scoped>
-.inline {
-  display: inline;
-}
-
 .memory {
   text-align: center;
   vertical-align: middle;
   line-height: 2.5rem;
-}
-
-.border {
-  border-radius: 10px;
-  display: inline-block;
-  //   box-shadow: 0px 0px 1px 0px black;
-  height: 45px;
-  width: 45px;
-  margin: 0.5rem;
   background-color: #f3f4f3;
-}
-
-.selected {
-  border-radius: 10px;
-  border: 1.5px solid green;
-}
-
-$height: 38px;
-
-.select {
-  margin: -42% auto;
-  /* display: inline-block; */
-  top: 50%;
-  position: relative;
-  width: $height;
-  height: $height;
-  border-radius: 7px;
-  -webkit-box-shadow: 0px 0px 1px 0px black;
-  box-shadow: 0px 0px 1px 0px black;
-  /* position: absolute; */
-  z-index: 0;
-  &:after {
-    content: '';
-    display: block;
-    position: absolute;
-    width: 100%;
-    height: 45%;
-    bottom: 0;
-    border-radius: 7px;
-  }
 }
 </style>
